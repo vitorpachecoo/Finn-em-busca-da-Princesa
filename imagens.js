1ffb000cefff4fe105c4c3e8a43a04a34f41c8f7
Vitor Pacheco <106789053+vitorpachecoo@users.noreply.github.com>
2022-09-18 23:03:33 -0300

    Create imagens.js

---
+++ imagens.js
@@ -0,0 +1,11 @@
+//Game Images
+
+let imagemPainel;
+let imagemDoAtor;
+let imagemInimigo;
+
+function preload(){
+  imagemPainel = loadImage("imagens/painel.jpg");
+  imagemDoAtor = loadImage("imagens/ator-1.png");
+  imagemInimigo = loadImage ("imagens/inimigo-1.png")
+}

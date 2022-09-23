28b9ff405092cc0c06907ec241e986aa24a508fb
Vitor Pacheco <106789053+vitorpachecoo@users.noreply.github.com>
2022-09-22 23:13:59 -0300

    Update inimigo.js
    - Ajustada a exibição e movimentação dos inimigos;
    - Adicionado a função realiza o looping dos inimigos;

--- inimigo.js
+++ inimigo.js
@@ -1,12 +1,31 @@
-//enemy code
-
-let xInimigo = 600;
-let yInimigo = 40;
+let xInimigos = [600, 600, 600, 600, 600, 600];
+let yInimigos = [100, 150, 210, 320, 380, 440];
+let velocidadeInimigos = [2, 2.5, 3.2, 5, 3.3, 3];
+let comprimentoInimigo = 50;
+let alturaInimigo = 50;
 
 function mostraInimigo(){
-  image(imagemInimigo, xInimigo, yInimigo, 60, 60);
+    for (let i = 0; i < imagemInimigos.length; i = i + 1){
+      image(imagemInimigos[i], xInimigos[i], yInimigos[i], comprimentoInimigo, alturaInimigo);
+  }
 }
 
 function movimentaInimigo(){
-  xInimigo -= 2;
+    for (let i = 0; i < imagemInimigos.length; i = i + 1){
+      xInimigos[i] -= velocidadeInimigos[i];
+  }
+}
+
+//volta Posição Inicial do Inimigo
+function reseta(){
+    for (let i = 0; i < imagemInimigos.length; i = i + 1){
+      if (passouTodaTela(xInimigos[i])){
+        xInimigos[i] = 600;
+    }
+  }
+}
+
+//retorna toda vez que o boneco passa tal coordenada
+function passouTodaTela(xInimigos){
+  return xInimigos < - 50;
 }
